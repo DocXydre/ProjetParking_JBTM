3bb40fdd649b7728870cdc7c959bca7854003d36
DocXydre <totolego88@gmail.com>
2024-10-09 10:43:31 +0200

    c bon la heu

---
+++ parking/src/utils/generateRandomNumberId.ts
@@ -0,0 +1,4 @@
+//  générer un nombre aléatoire, entier et positif, contenant 6 chiffres.
+export const generateRandomNumberId = (): number => {
+    return Math.floor(Math.random() * 900000) + 100000;
+  };
