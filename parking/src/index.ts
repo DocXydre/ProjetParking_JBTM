981483fd8eb056ebeafd2e751e23fedb48d87e1e
DocXydre <74959654+DocXydre@users.noreply.github.com>
2024-10-29 22:51:58 +0100

    test

--- parking/src/index.ts
+++ parking/src/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from 'hono'
 
-import { toSlug } from '../utils/toSlug';
+import { toSlug } from './utils/toSlug';
 
 const slug = toSlug("Mon exemple de chaîne à convertir!");
 console.log(slug); // Affiche: mon-exemple-de-chaine-a-convertir
