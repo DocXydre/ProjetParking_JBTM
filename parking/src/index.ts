e3fb283f8f61b5390c6c9c980e4152ca2c1c8963
Juth-a <justin.burr7@etu.univ-lorraine.fr>
2024-11-13 08:04:07 +0100

    Changements

--- parking/src/index.ts
+++ parking/src/index.ts
@@ -2,14 +2,14 @@ import { Hono } from 'hono'
 import { HomeController } from './controllers/HomeController';
 import parkingRoutes from './routes/parkingRoutes';
 import cityRoutes from './routes/cityRoutes';
-import { serveStatic } from '@hono/node-server/serve-static';
+import { serveStatic } from 'hono/bun';
 import { staticDatabase } from './data/staticDatabase';
 
 const app = new Hono()
 const cities = staticDatabase.cities;
 const parkings = staticDatabase.parkings;
 
-app.use('/static/*', serveStatic({ root: './' }));
+app.use("/static/*", serveStatic({ root: "./" }));
 
 app.get('/', HomeController)
 app.route('/parkings', parkingRoutes);
